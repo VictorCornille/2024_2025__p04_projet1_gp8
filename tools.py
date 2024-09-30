15be2274a11e67384446d774514d4d8682a75999
taki-winie <cyprien.carlot@icloud.com>
2024-09-30 11:02:45 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,5 +1,6 @@
-def  ask_for_the_init_number(chain):
-    pass
+from data import*
+def  ask_for_the_init_number(init_number):
+    if init_number is bin_valide_char
 
 def ask_for_the_init_base ():
     pass
