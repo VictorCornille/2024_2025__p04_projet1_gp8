2dedc6ffc84976f74298f782642f14fbbcf1ea4f
taki-winie <cyprien.carlot@icloud.com>
2024-09-30 12:02:16 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
 bin_valide_char = ["0","1"]
 dec_valide_char = ["0","1","2","3","4","5","6","7","8","9"]
-hex_valiide_char =  dec_valide_char + ["A","B","C","D","E","F"]
+hex_valide_char =  dec_valide_char + ["A","B","C","D","E","F"]
+ask_for_init_number_text = "file moi le nombre:"
+ask_again_for_init_number_text = "file moi encore un nombre:"
