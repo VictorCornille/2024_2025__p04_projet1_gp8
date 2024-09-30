f72b8143557fa002af26caeed8c371d2b932cbd8
taki-winie <cyprien.carlot@icloud.com>
2024-09-30 10:20:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def bin_dec_hex__to__bin_dec_hex (init_number,\
                                    init_base,\
                                       target_base):
-    pass
+    
     target_number = None
     return target_number
 
-from utils import *
+from tools import *
 
 assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 
