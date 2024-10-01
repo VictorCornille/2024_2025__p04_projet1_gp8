cf0016b378664a6635e815e76fceac294d2e0184
victorcauss1 <vikosbb08@gmail.com>
2024-10-01 09:12:01 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,18 +1,37 @@
+from data import*
 from main import*
-# def char_binaire (init_number):
-#     # char_is_valid=True
-#     # while char_is_valid:
-#     #     char_is_valid = init_number[1] hex_valide_char
-#     #     i=i+1
-#         while init_number > 0:
-#              init_number % 2
-#         return
+def check_valid_char(char):
+       return char in hex_valid_char
+       
+def is_a_valid_number(number):
+    i=0
+    while is_valid_char:
+        is_valid_char = check_char_validity (number[i])
+        i= i +1   
+    return is_valid_char   
+                      
+
+def ask_for_the_init_number(init_number):
+    init_number = input (ask_for_init_number_text)
+    while not ( a_valid_number (init_number)):
+        init_number = input (ask_again_for_init_number_text)
 
 
-     
 
-# def ask_for_the_init_base ():
-#     pass
 
-# def ask_for_the_target_base():
-#     pass
+
+def ask_for_the_init_base ():
+#    si binaire alors =2
+#    si decimal alors =10
+#    si hexadecimal alors =16
+    
+       
+
+ def ask_for_the_target_base():
+     pass
+#  Fonction 1 qui passe de binaire a decimal
+#  Fonction 2 qui passe de binaire a hexa
+#  Fonction 3 qui passe de decimal a binaire (l'inverse de la fonction 1)
+#  Fonction 4 qui passe de decimal a hexa
+#  Fonction 5 qui passe de hexa a binaire (l'inverse de la fonction 2)
+#  Fonction 6 qui passe de hexa a decimal (l'inverse de la fonction 4)
