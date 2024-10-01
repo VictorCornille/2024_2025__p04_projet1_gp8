9540b593ffd33026d668e721e69407c2e778bee4
taki-winie <cyprien.carlot@icloud.com>
2024-10-01 09:55:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def bin_dec_hex__to__bin_dec_hex (init_number,\
 # assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 
 def do_the_job ():
-    init_number = ask_for_the_init_number ()
+    init_number = ask_for_the_init_number()
     init_base = ask_for_the_init_base ()
     target_base = ask_for_the_target_base ()
     target_number = \
