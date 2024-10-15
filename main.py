0b5a91e4effef37d9527b1f5e47c2d759cd9e003
taki-winie <cyprien.carlot@icloud.com>
2024-10-15 09:46:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,18 @@ def do_the_job ():
                                     init_base, \
                                     target_base)
 
+def do_the_job():
+    init_number = ask_for_the_init_number()
+    init_base = ask_for_the_init_base()
+    target_base = ask_for_the_target_base()
+    
+    if init_base is None or target_base is None:
+        print("Choix de base invalide.")
+        return
+    
+    target_number = bin_dec_hex_to_bin_dec_hex (init_number, init_base, target_base)
+    
+    if target_number is not None:
+        print(f"Le nombre {init_number} en base {init_base} est {target_number} en base {target_base}.")
+    else:
+        print("Erreur dans la conversion.")
