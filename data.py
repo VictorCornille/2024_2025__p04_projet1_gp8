ef4e6a9459c4909f6bd767a5dc1ac5f6e16edfae
taki-winie <cyprien.carlot@icloud.com>
2024-11-04 11:52:37 +0100

    Update data.py

--- data.py
+++ data.py
@@ -4,6 +4,5 @@ dec_valide_char = ["0","1","2","3","4","5","6","7","8","9"]
 hex_valid_char =  dec_valide_char + ["A","B","C","D","E","F"]
 ask_for_init_number_text = "file moi le nombre:"
 ask_again_for_init_number_text = "file moi encore un nombre:"
-
-
-
+valid_number = dec_valide_char + ["A","B","C","D","E","F"]
+valid_chars = hex_valid_char
